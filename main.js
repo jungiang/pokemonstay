0ef97d65a98f319bfbe0e067b0598eca5d033a07
Jun Giang <jungiang@yahoo.com>
2019-06-09 16:28:39 -0700

    resized and positioned elements surround tutorial image so tutorial doesn't break

--- main.js
+++ main.js
@@ -382,12 +382,10 @@ function goToBed(){
 var slideIndex = 1;
 showSlides(slideIndex);
 
-// Next/previous controls
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-// Thumbnail image controls
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
